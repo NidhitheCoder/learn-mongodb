84c8466ac63268acf81ff1e897f9c944f3e412bd
NidhitheCoder <nidhpv@gmail.com>
2021-08-04 22:14:30 +0530

    course route written in separate folder named controller

--- mongo-with-node/index.js
+++ mongo-with-node/index.js
@@ -6,15 +6,28 @@ const path = require('path');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
+const courseController = require('./controllers/courses')
+
 application.use(bodyParser.urlencoded({
   extended: true,
 }));
 
 application.set('views', path.join(__dirname, 'views'));
 
+application.engine('hbs', expressHandlebars({
+  extname: 'hbs',
+  defaultLayout: 'mainlayout',
+  layoutsDir: __dirname + "/views/layouts"
+}));
+
+application.set('view engine', 'hbs');
+
 application.get('/', (req, res) => {
-  res.send('<H1>Welcome to node with mongo project</H2>')
+  // res.send('<H1>Welcome to node with mongo project</H2>')
+  res.render("index", {});
 });
+
+application.use('/course', courseController);
 
 application.listen('5000', () => {
   console.log('Server Started');
